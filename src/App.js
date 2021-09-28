b6646df8e73c82d44d04a87c6e88ef0e4deb66fe
juanpablomathieu <juanpablomathieu@gmail.com>
2021-09-28 15:36:52 -0300

    added auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. THIS IS V2
+          <h1>We now have Auth!</h1>
         </p>
         <a
           className="App-link"
@@ -18,8 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
