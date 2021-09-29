2ef5039f2b53f1d176fba91463fc91ad9e28c1e6
juanpablomathieu <juanpablomathieu@gmail.com>
2021-09-29 18:31:13 -0300

    added notes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -24,7 +24,11 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://cnw6nrrngjcmndr5wb5o7yqota.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-ecul3bw4qnc6rg7tlkfvyash7e"
 };
 
 
